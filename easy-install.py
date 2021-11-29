2dca95eeb6113cc6c948550f37d8b4e9afce9027
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-11-29 09:18:26 +0100

    Initial commit of the easy install script written in python

---
+++ easy-install.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Easy installation script for the WISdoM OSS platform. Since we need to generate
+some passwords. Some python packages will be installed. This may be done while
+executing the install.sh file
+"""
+import secrets
+

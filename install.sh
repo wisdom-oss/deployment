369f12c1dd51fc15bfc1f94f0880fb112059d3e1
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-05 16:00:17 +0100

    Added warning about errors being output due to error in docker compose binary

--- install.sh
+++ install.sh
@@ -193,6 +193,8 @@ then
   read -rp "Hostname des Servers: " hostname
   $sudo sed -i "s,<<hostname>>,$hostname,g" data/caddy/Caddyfile
   echo -e "\n${lightblue}Starting the containers${nocolor}\n"
+  echo -e "${yellow}WARNING: There will be some images marked as error. This is normal and does not interfere"
+  echo -e "with the actual deployment of the containers.${normal}"
   $sudo docker compose up -d || echo -e "\n${red}Error while staring the containers${nocolor}\n" && exit 1
   sleep 30s
   $sudo docker restart wisdom-oss-api-gateway-1

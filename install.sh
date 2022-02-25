c6d790af19122562581a476fd869a0788132f587
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-02-25 10:28:16 +0100

    Added a postgres (with postgis extensions) db

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ ROOT_DIRECTORY="/opt/wisdom-oss"
 BRANCH="main"
 
 # Mapping of passwords which shall be generated and in which file it may be needed
-password_blanks=("gen-db-pass" "gen-pass-rabbitmq")
+password_blanks=("gen-db-pass" "gen-pass-rabbitmq" "gen-postgres-pass")
 
 # Location of the docker-compose file relative to the current directory
 compose_file_location="./docker-compose.yml"
--- update.sh
+++ update.sh
@@ -32,7 +32,7 @@ normal=$(tput sgr0)
 
 ROOT_DIRECTORY="/opt/wisdom-oss"
 # Mapping of passwords which shall be generated and in which file it may be needed
-password_blanks=("gen-db-pass" "gen-pass-rabbitmq")
+password_blanks=("gen-db-pass" "gen-pass-rabbitmq" "gen-postgres-pass")
 
 # Location of the docker-compose file relative to the current directory
 compose_file_location="./docker-compose.yml"

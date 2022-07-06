265df33e821204e73e3ebca5d41b9a7d66ff9951
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-07-06 14:07:52 +0200

    Error in script not preparing the database for kong

--- update-dev.sh
+++ update-dev.sh
@@ -132,8 +132,8 @@ echo -e "${cyan}2. Creating new Docker Images${nocolor}\n"
 $sudo docker compose -f docker-compose.dev.yml build
 echo -e "\n${green}✅ Successfully created new docker images${nocolor}\n"
 
-if [[ -f "./.tokens/.kong-prepared" ]]; then
-  echo -e "${cyan}3. Preparing databases${nocolor}\n"
+if [[ ! -f "./.tokens/.kong-prepared" ]]; then
+  echo -e "${lightgreen}3. Preparing databases${nocolor}\n"
   $sudo docker compose -f docker-compose.dev.yml up -d postgres
   $sudo docker run --rm --network=wisdom-oss --env-file ./kong-environment.env kong/kong-gateway:2.8.1.1-alpine kong migrations bootstrap
   $sudo touch ./.tokens/.kong-prepared

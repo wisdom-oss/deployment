a9b6e14f75d6c71336c0b3106fca9a7039bc8902
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-01-16 11:00:29 +0100

    chore: adding correct .env setup

--- update.sh
+++ update.sh
@@ -90,6 +90,9 @@ echo -e "\n${green}✅ Pulled from the deployment repository${nocolor}\n"
 if [[ -e ".env" ]]; then
   echo -e "${cyan}Setup was already executed${normal}"
 else
+  password_blanks=("postgres-user" "postgres-pass" "authentik-secret-key" "authentik-admin-pass" "authentik-admin-api-token")
+  frontend_binding="frontend-binding"
+  authentik_binding="authentik-binding"
   echo -e "${orange}Using ${bold}$branch ${normal}${orange} version of the project${normal}"
   $sudo cp .env-template .env
   echo -e "${lightgreen}Generating secrects with openssl${normal}"
@@ -102,6 +105,72 @@ else
         $sudo sed -i "s/<<$field>>/$(openssl rand -hex 16)/g" .env
       fi
   done
+
+  echo -e "\n${lightpurple}Authentik Setup${normal}"
+echo -e "The platform uses authentik to authorize users in the frontend. Please select an apropriate"
+echo -e "access method to authentik:"
+echo -e "1) Access authentik on own domain"
+echo -e "2) Access authentik on own port (recommended on internal deployments)"
+
+while :
+do
+read -rp "Please select a installation method [2]: " option
+
+if [[ $option == 1  ]]
+then
+  echo -e "Please enter the hostname which shall be used to access the authentik frontend"
+  while :
+  do
+    echo -en "Binding for Authentik: "
+    read -r hostname
+    echo -en "You entered \"${hostname}\". Is this correct? [Y/n]: "
+    read -r confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] || $confirm == "" ]]
+    then
+      $sudo sed -i "s/<<${authentik_binding}>>/$hostname/g" .env
+      break
+    fi
+  done
+  break
+elif [[ $option == 2 || $option = "" ]]
+then
+  echo -e "The authentik container will be available on the port 8080"
+  $sudo sed -i "s/<<${authentik_binding}>>/:8080/g" .env
+  break
+fi
+done
+
+echo -e "\n${lightpurple}Frontend${normal}"
+echo -e "You have two installation options for the frontend:"
+echo -e "1) Installing the system for intranet usage (Recommended for testing purposes) [${green}Standard${normal}]"
+echo -e "2) Installing the system for internet usage (Recommended for deployment purposes, ⚠️ ${yellow}HTTPS Only${normal} ⚠️ )\n"
+
+while :
+do
+read -rp "Please select a installation method [1]: " option
+
+if [[ $option == 1 || $option = "" ]]
+then
+  $sudo sed -i "s/<<${frontend_binding}>>/:80/g" .env
+  break
+elif [[ $option == 2 ]]
+then
+  echo -e "Please enter the hostname unter which the dashboard shall be made available."
+  echo -e "⚠️ ${yellow}The entered hostname needs to match the address used in the browser exactly${normal}\n"
+  while :
+  do
+    echo -en "Binding for the frontend: "
+    read -r hostname
+    echo -en "You entered \"${hostname}\". Is this correct? [Y/n]: "
+    read -r confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] || $confirm == "" ]]
+    then
+          $sudo sed -i "s/<<${frontend_binding}>>/${hostname}/g" .env
+      break
+    fi
+  done
+fi
+done
 fi
 echo -e "\n${green}✅ Generated secrets${nocolor}\n"
 
